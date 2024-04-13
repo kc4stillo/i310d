d413b7259d64462e09704c24fc5da087daa918ca
kyle c <163612025+kc4stillo@users.noreply.github.com>
2024-04-12 20:12:13 -0500

    Create volume_of_sphere.py find volume

---
+++ volume_of_sphere.py
@@ -0,0 +1,9 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    return (4/3) * pi * (radius ** 3)
+
+def main():
+    print(calculate_volume_of_sphere(30))
+    print(calculate_volume_of_sphere(40))
+    
+main()
